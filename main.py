d8983db20aeaba4a58bfa22ddfc2f0cf1afcc914
Venkatesh nellore <51104553+samflynn007@users.noreply.github.com>
2022-03-14 14:48:35 +0000

    project 1 finish

---
+++ main.py
@@ -0,0 +1,5 @@
+print("Welcome to the band name generator")
+city_name = input("what's name of the city you grew up in?\n")
+pet_name = input("what's your pet's name?\n")
+print("your band name could be "+ city_name + " " + pet_name)
+print("your band name could be {} {}".format(city_name,pet_name))
